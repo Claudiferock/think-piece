0a52c726426fda2902551704cb502a7bb7487884
Claudio <claudifero@gmail.com>
2019-12-16 09:07:05 +0200

    Add signUp

--- src/firebase.js
+++ src/firebase.js
@@ -26,4 +26,30 @@ const firebaseConfig = {
 
   window.firebase = firebase;
 
+  export const createUserProfileDocument = async (user, additionalData) => {
+    if (!user) return;
+
+    // Get a reference to the place in the db where a user profile might be.
+    const userRef = firestore.doc(`users/${user.uid}`);
+
+    // Go and fetch the document from that location
+    const snapshot = await userRef.get();
+
+    if (!snapshot.exists) {
+      const { displayName, email, photoURL } = user;
+      const createdAt = new Date();
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          photoURL,
+          createdAt,
+          ...additionalData,
+        })
+      } catch (error) {
+        console.error('Error creating user', error);
+      }
+    }
+  };
+
   export default firebase;
